32a7d2348413866a6d9e90002897adcd1443d3be
playtvapk <playtvliveapk@gmail.com>
2024-08-06 13:04:00 -0300

    Create ESPN.php

---
+++ ESPN.php
@@ -0,0 +1,53 @@
+<!--PLAYER--><!DOCTYPE html><html lang="pt_BR"><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+   margin: 0;
+   padding: 0;
+}
+#player {
+position: absolute;
+top: 0;
+left: 0;
+bottom: 0;
+height: 100%;
+width: 100%;
+border: 0;
+overflow: hidden;
+}
+
+</style>
+<meta name="referrer" content="no-referrer">
+<script src="js/KB5zFt7A.js"></script>
+<script> jwplayer.key='XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';</script>
+</head>
+<body>
+<div id="player"></div>
+<script>
+var playerInstance = jwplayer("player");
+playerInstance.setup({
+	playlist: [{
+		"sources": [
+			{
+				"default": false,
+				"type": "dash",
+				"file": "https://dplus.cdn-4d1.workers.dev/gru1/va01/disneyplus/channel/ef857a31-bd76-436b-b327-8889d90e6e5c-1721255046217/ctr-all-sliding.m3u8",
+				"drm": {
+					"clearkey": { "keyId": "f1760380ec6d449db16a6b9121a6e11a", "key": "44ff8d71a7e7924b185f85dfa5d49fdc" }
+				},
+				"label": "0"
+			}
+		]
+	}],
+	width: "100%",
+	height: "100%",
+	aspectratio: "16:9",
+	autostart: true,
+	cast: {},
+	sharing: false
+});
+</script>
+<script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'8af03a214a766727',t:'MTcyMjk1OTg5NS4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script>
+
+</body></html><!--PLAYER-->
